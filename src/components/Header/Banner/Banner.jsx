1b4db0b288bdebfce55aed58747bf742dd622c66
princesaha007 <princeroysaha111@gmail.com>
2023-09-27 21:28:22 +0600

    changed the opacity in the background image of the banner section

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
 <>
   <div className='h-[60vh] my-8 relative'>
     <img src={bannerImage} alt="" className="mb-[-20px] sm:mb-0" />
-    
+    <div className="absolute top-0 bottom-0 w-full h-[66vh] bg-white opacity-90"></div>
   </div>
   <div className="absolute top-44 sm:top-52 md:top-64 lg:top-64 font-bold md:left-[10px] sm:left-0 text-center w-full">
     <p className="text-lg sm:text-lg md:text-5xl lg:text-5xl">I Grow By Helping People In Need</p>
